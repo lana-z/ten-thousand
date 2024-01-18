a2d1b0cac9f1dd234dc4af2ff52d2b0be0755f75
lana-z <lana@levelupeconomy.com>
2024-01-17 18:56:15 -0800

    added one and done

--- ten_thousand/game.py
+++ ten_thousand/game.py
@@ -1,15 +1,35 @@
-
+from ten_thousand.game_logic import GameLogic 
 
 def play():
     print("Welcome to Ten Thousand")
-    choice = input("(y)es to play or (n)o to decline\n> ")
+    play_game = input("(y)es to play or (n)o to decline\n> ")
 
-    if choice.lower() == 'y':
+    if play_game.lower() == 'y':
         # Add logic here to start the game
-        print("Starting the game...🎲")
-    elif choice.lower() == 'n':
+        print("Starting round 1 🏁")
+        
+        print("Rolling 6 dice...🎲")
+        dice_roll = GameLogic.roll_dice(6)
+        print("***", " ".join(map(str, dice_roll)), "***")
+        
+        keep_dice = input("Enter dice to keep, or (q)uit:\n> ")
+        if keep_dice.lower() == 'q':
+            print("Thanks for playing. You earned 0 points")
+
+
+    elif play_game.lower() == 'n':
         print("OK. Maybe another time")
     else:
         print("Invalid input. Please enter 'y' or 'n'.")
 
-play()
+if __name__ == "__main__":
+    play()
+
+def one_and_done():
+    print("Rolling 6 dice...")
+    dice_roll = GameLogic.roll_dice(6)
+    print("***", " ".join(map(str, dice_roll)), "***")
+    print("Enter dice to keep, or (q)uit:")
+
+    if choice.lower() == 'q':
+        print("Thanks for playing. You earned 0 points")
