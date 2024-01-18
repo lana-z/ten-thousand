a611c131e72b3768d2f56390779323bb45d993d1
lana-z <lana@levelupeconomy.com>
2024-01-17 18:12:53 -0800

    added play function with welcome and y/nto start

--- ten_thousand/game.py
+++ ten_thousand/game.py
@@ -1,2 +1,15 @@
+
+
 def play():
-    
+    print("Welcome to Ten Thousand")
+    choice = input("(y)es to play or (n)o to decline\n> ")
+
+    if choice.lower() == 'y':
+        # Add logic here to start the game
+        print("Starting the game...🎲")
+    elif choice.lower() == 'n':
+        print("OK. Maybe another time")
+    else:
+        print("Invalid input. Please enter 'y' or 'n'.")
+
+play()
