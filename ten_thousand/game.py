61beb3cc64734a9adfc822cd575a7fce06a65cef
lana-z <lana@levelupeconomy.com>
2024-01-18 22:45:40 -0800

    added some things and broke some things

--- ten_thousand/game.py
+++ ten_thousand/game.py
@@ -11,46 +11,60 @@ def play(roller=None):
 
     if play_game.lower() == 'y':
         round_number = 1
-        
         # Flag to indicate the end of the game
         user_quit = False
 
-        #while True: the user keeps playing is true, infinite loop
-        #while not user_quit
-        #while user_quit == False  do same thing but the second two include a Flag for end of game and prevent infinite loop
-        #all three of the above are the same the second two include a Flag, a way to let the user quit and stop the loop
         while not user_quit:
             print(f"Starting round {round_number}")
-        
-            print("Rolling 6 dice...")
-            dice_roll = roller(6)
-            print("*** " + ''.join(str(die) + ' ' for die in dice_roll).strip() + " ***")
-        
-            while True:
-                keep_dice = input("Enter dice to keep, or (q)uit:\n> ")
-
-                if keep_dice.lower() == 'q':
-                    user_quit = True
+            dice_remaining = 6   
+            #reset round score to 0 at the start of each round    
+            unbanked_score = 0 
+            
+            while dice_remaining > 0:
+                print(f"Rolling {dice_remaining} dice...")
+                dice_roll = roller(dice_remaining)
+                print("*** " + ''.join(str(die) + ' ' for die in dice_roll).strip() + " ***")
+            
+                roll_score = GameLogic.calculate_score(dice_roll)
+                if roll_score == 0:  # Check for Farkle
+                    print("""
+                    ****************************************
+                    **        Zilch!!! Round over         **
+                    ****************************************
+                    """)
                     break
-                
-                else:
-                    #convert each character in keep_dice to an integer
-                    keep_dice = tuple(map(int, keep_dice))
-                    unbanked_score = GameLogic.calculate_score(keep_dice)
-                    dice_remaining = 6 - len(keep_dice)
 
-                    next_roll = input(f"You have {unbanked_score} unbanked points and {dice_remaining} dice remaining\n(r)oll again, (b)ank your points or (q)uit:\n> ")
+                while True:
+                    keep_dice = input("Enter dice to keep, or (q)uit:\n> ")
+
+                    if keep_dice.lower() == 'q':
+                        user_quit = True
+                        break
+                
+                    else:
+                        #convert each character in keep_dice to an integer
+                        keep_dice = tuple(map(int, keep_dice))
+                        unbanked_score = GameLogic.calculate_score(keep_dice)
+                        dice_remaining = 6 - len(keep_dice)
+
+                        next_move = input(f"You have {unbanked_score} unbanked points and {dice_remaining} dice remaining\n(r)oll again, (b)ank your points or (q)uit:\n> ")
                     
-                    if next_roll.lower() == 'b':
+                    if next_move.lower() == 'b':
                         score += unbanked_score
                         print(f"You banked {unbanked_score} points in round {round_number}")
                         print(f"Total score is {score} points")
+                        round_number += 1
                         break
 
-                    elif next_roll.lower() == 'r':
-                        break
+                    elif next_move.lower() == 'r':
+                        print(f"Rolling {dice_remaining} dice...")
+                        dice_roll = roller(dice_remaining)
+                        print("*** " + ''.join(str(die) + ' ' for die in dice_roll).strip() + " ***")
+                        dice_remaining -= len(keep_dice)
+                        
+                        roll_score = GameLogic.calculate_score(dice_roll)
 
-                    elif next_roll.lower() == 'q':
+                    elif next_move.lower() == 'q':
                         user_quit = True
                         print(f"Thanks for playing. You earned {score} points")                        
                         break
@@ -60,8 +74,6 @@ def play(roller=None):
             
             if user_quit:
                 break
-
-            round_number += 1
 
         print(f"Thanks for playing. You earned {score} points")
 
